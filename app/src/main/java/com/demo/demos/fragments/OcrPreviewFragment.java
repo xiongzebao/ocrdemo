9ec79cdbe1a42f98a8a0615125b232ce22ac3457
xiongbin <923450980@qq.com>
2021-03-09 08:58:33 +0800

    first commit

---
+++ app/src/main/java/com/demo/demos/fragments/OcrPreviewFragment.java
@@ -0,0 +1,17 @@
+package com.demo.demos.fragments;
+
+import com.demo.demos.model.ScanResult;
+
+/**
+ * @author xiongbin
+ * @description:
+ * @date : 2021/3/9 8:48
+ */
+
+public class OcrPreviewFragment extends PreviewFragment {
+
+    @Override
+    protected ScanResult ocrRecognize(byte[] image) {
+        return super.ocrRecognize(image);
+    }
+}
---
+++ app/src/main/java/com/demo/demos/model/ScanResult.java
@@ -0,0 +1,13 @@
+package com.demo.demos.model;
+
+/**
+ * @author xiongbin
+ * @description:
+ * @date : 2021/3/9 8:50
+ */
+
+public class ScanResult {
+    public static final  int SUCCESS=1;
+    public int code;
+    public String message;
+}
